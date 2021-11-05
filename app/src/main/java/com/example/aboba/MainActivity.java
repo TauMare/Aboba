b67a225400474d82647899ea7dd7bc3af71d2bb8
TauMare <46701160+TauMare@users.noreply.github.com>
2021-11-05 03:28:17 +0300

    Начинаем работку

--- app/src/main/java/com/example/aboba/MainActivity.java
+++ app/src/main/java/com/example/aboba/MainActivity.java
@@ -3,20 +3,12 @@ package com.example.aboba;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
-    Integer counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void sendMessage(View view) {
-        Button button = findViewById(R.id.btn1);
-        counter++;
-        button.setText(counter.toString());
     }
 }
