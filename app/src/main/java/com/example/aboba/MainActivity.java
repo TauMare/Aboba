3de677d4c3a40ab5369595a44d87fee38a3f1f5f
TauMare <46701160+TauMare@users.noreply.github.com>
2021-11-05 04:11:31 +0300

    шо то вроде работает

--- app/src/main/java/com/example/aboba/MainActivity.java
+++ app/src/main/java/com/example/aboba/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.aboba;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void btn_click(View view)
+    {
+        ImageView img = (ImageView)findViewById(R.id.imageView);
+        img.setBackgroundResource(R.drawable.anim);
+        AnimationDrawable frameAnimation = (AnimationDrawable) img.getBackground();
+        frameAnimation.setOneShot(false);
+        frameAnimation.start();
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setText("stop");
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(frameAnimation.isRunning()) {
+                    frameAnimation.stop();
+                    btn.setText("start");
+                }
+                else {
+                    frameAnimation.start();
+                    btn.setText("stop");
+                }
+            }
+        });
     }
 }
